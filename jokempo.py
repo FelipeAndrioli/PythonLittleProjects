262f84bad7d0fa0eb6c020bb20d7c134b8ee66f9
Felipe Andrioli <felipeandrioli56.fa@gmail.com>
2020-03-04 00:17:00 -0300

    Jokempo finished

--- jokempo.py
+++ jokempo.py
@@ -4,31 +4,30 @@ from random import randint
 
 def ComputerMove():
 
-    move = [["Paper"],
-            ["Scissor"],
-            ["Rock"]]
+#    move = [[1, "Paper"],
+#            [2, "Scissor"],
+#            [3. "Rock"]]
 
-    return move[randint(0, 2)]
+    return randint(1, 3)
 
 def GameRules(userMove, computerMove):
 
     #Victory = 1 - Computer Win!
-    #Victory = 2 - Tie!
     #Victory = 3 - User Win!
 
     if (userMove == computerMove):
         return 2
-    elif (userMove == 0 and computerMove == 1):
-        return 1
-    elif (userMove == 1 and computerMove == 0):
-        return 3
-    elif (userMove == 1 and computerMove == 2):
+    elif (userMove == 1 and computerMove == 3):
         return 1
     elif (userMove == 2 and computerMove == 1):
         return 3
-    elif (userMove == 0 and computerMove == 2):
+    elif (userMove == 2 and computerMove == 3):
+        return 1
+    elif (userMove == 3 and computerMove == 2):
         return 3
-    elif (userMove == 2 and computerMove == 0):
+    elif (userMove == 1 and computerMove == 3):
+        return 3
+    elif (userMove == 3 and computerMove == 1):
         return 1
 
 def ConvertWinner(winner):
@@ -41,15 +40,51 @@ def ConvertWinner(winner):
 
 def ValidOption(inputOption):
 
-    if(inputOption <= 2 and inputOption > 0):
+    if(inputOption <= 3 and inputOption > 0):
         return True
     else:
         return False
 
-userMove = 0
+def ReturnMove(action):
+
+    move = [[""],
+            ["Paper"],
+            ["Scissor"],
+            ["Rock"]]
+
+    return move[action]
+
+def main():
+
+    userMove = 0
+    computerMove = ComputerMove()
 
 
-try:
-    userMove = int(input("Make your move: "))
-except:
-    print("You should make an valid move!")
+    while(ValidOption(userMove) == False):
+        print("1 - Paper")
+        print("2 - Scissor")
+        print("3 - Rock")
+        print("JO")
+        print("KEM")
+        print("PO!")
+    
+        try:
+            userMove = int(input("Make your move: "))
+        except:
+            print("You should make an valid move!")
+
+    print("Computer action: " + str(ReturnMove(computerMove)))
+    print("Your action: " + str(ReturnMove(userMove)))
+    print(ConvertWinner(GameRules(userMove, computerMove)))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
